baaeb16ae2a4f0e34cbcce825f91d40bd33ce451
Tetiana Ilienko <tatyanailyenko@gmail.com>
2023-11-27 18:14:49 +0100

    update HW

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,19 +5,22 @@ import React, { useState } from "react";
 const Counter = () => {
 
 
-    const buttonClick = (countNum) => {
-        setCount((prevCount) => prevCount + countNum);
-    };
+    // const buttonClick = (countNum) => {
+    //     setCount((prevCount) => prevCount + countNum);
+    // };
+    
+    const changeCountByBtn = (x) => setCount(count +x);
+    
 
-        
     const [count, setCount] = useState(0);
         return (
         <>
             <h2>Counter: {count}</h2>
-            <button onClick={() => buttonClick(-1)}>Decrement -1</button>
-            <button onClick={() => buttonClick(+1)}>Increment +1</button>
-            <button onClick={() => buttonClick(-10)}>Decrement -10</button>
-            <button onClick={() => buttonClick(+10)}>Increment +10</button>
+            
+            <button onClick={() => changeCountByBtn(-1)}>Decrement -1</button>
+            <button onClick={() => changeCountByBtn(+1)}>Increment +1</button>
+            <button onClick={() => setCount(count-10)}>Decrement -10</button>
+            <button onClick={() => changeCountByBtn(+10)}>Increment +10</button>
         </>
     );
 }
